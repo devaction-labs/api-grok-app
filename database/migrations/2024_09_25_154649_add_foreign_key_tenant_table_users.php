3d758929246825a3360e97456b065d445ff8a864
Alex Nogueira <alex@devaction.com.br>
2024-09-25 13:11:28 -0300

    feat(sanctum): add Laravel Sanctum support and migrations

--- database/migrations/2024_09_25_154649_add_foreign_key_tenant_table_users.php
+++ database/migrations/2024_09_25_154649_add_foreign_key_tenant_table_users.php
@@ -12,7 +12,7 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignUlid(Tenant::class)->constrained();
+            $table->foreignIdFor(Tenant::class)->constrained();
         });
     }
 
